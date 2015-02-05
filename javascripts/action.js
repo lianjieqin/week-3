019e925b6628e5e62bcde30e14463f507232f5a7
Lianjie Qin <ljqin@thoughtworks.com>
2015-02-05 17:41:30 +0800

    [qlj] 2

---
+++ javascripts/action.js
@@ -0,0 +1,17 @@
+
+
+function change_class(id)
+{
+    id.className="animated shake";
+}
+
+function remove_class(id)
+{
+    id.className="";
+}
+
+function change_class1(id)
+{
+    id.className="animated pulse";
+}
+
